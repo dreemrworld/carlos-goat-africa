dfb129692e3c566818bf8ec73578b2239e81ce9a
carlos <dreemrworld@gmail.com>
2025-07-08 23:58:45 +0100

    Fix spacing in footer text

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/button';
 export const Footer = () => {
   return (
     <footer className="text-muted-foreground my-2 text-sm">
-      Created by
+      Created by{' '}
       <Button variant="link" className="text-primary p-0 font-medium">
         <a href="https://www.goat.africa/carlos-araujo">Carlos Araújo</a>
       </Button>
